4628a97e681c79bfa5f09c26cc8a2eedd9212e4b
Patrick Skillen <pskillen@gmail.com>
2025-03-20 11:20:24 +0000

    build: run migrations on rpi when deploying via script

--- deploy/scripts/deploy_rpi.sh
+++ deploy/scripts/deploy_rpi.sh
@@ -4,3 +4,12 @@ script_dir="$(dirname "$0")"
 project_dir="$(realpath "${script_dir}/../..")"
 
 rsync -av --exclude-from="${project_dir}/.rsyncignore" "${project_dir}/" paddy@meshcontrol.local:~/MeshtasticAPI
+
+
+# Run the migration script on the remote server
+ssh paddy@meshcontrol.local << 'EOF'
+    cd ~/MeshtasticAPI
+    source venv/bin/activate
+    cd MeshtasticBotManager
+    python manage.py migrate
+EOF
