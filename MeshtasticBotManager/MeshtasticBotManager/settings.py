88c3bae1ba9dba74562127abc787236d368aa70e
Patrick Skillen <pskillen@gmail.com>
2025-03-21 13:21:16 +0000

    feat: log request data if we're sending a 400 BadRequest on /api/raw-packet

--- MeshtasticBotManager/MeshtasticBotManager/settings.py
+++ MeshtasticBotManager/MeshtasticBotManager/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "MeshtasticBotManager.middleware.LogBadRequestMiddleware",
 ]
 
 ROOT_URLCONF = "MeshtasticBotManager.urls"
