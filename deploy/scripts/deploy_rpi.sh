7d9e649cfba62f2fb7d83d08d1dd92310b968773
Patrick Skillen <patrick.skillen2@nhs.scot>
2025-03-25 13:02:53 +0000

    build(local): Parameterise deploy script

--- deploy/scripts/deploy_rpi.sh
+++ deploy/scripts/deploy_rpi.sh
@@ -3,12 +3,14 @@
 script_dir="$(dirname "$0")"
 project_dir="$(realpath "${script_dir}/../..")"
 
-rsync -av --exclude-from="${project_dir}/.rsyncignore" "${project_dir}/" paddy@meshcontrol.local:~/MeshtasticAPI
+remote_dir="~/MeshtasticAPI"
+
+rsync -av --exclude-from="${project_dir}/.rsyncignore" "${project_dir}/" paddy@meshcontrol.local:$remote_dir
 
 
 # Run the migration script on the remote server
-ssh paddy@meshcontrol.local << 'EOF'
-    cd ~/MeshtasticAPI
+ssh paddy@meshcontrol.local << EOF
+    cd $remote_dir
     source venv/bin/activate
     pip install -r requirements.armv7.txt
     cd MeshtasticBotManager
