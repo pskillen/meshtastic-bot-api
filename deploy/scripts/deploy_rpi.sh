e75770c06c7deaff279a9f929fe400ee7553da78
Patrick Skillen <patrick.skillen2@nhs.scot>
2025-03-25 23:22:04 +0000

    deploy: pip install first, then copy new server code

--- deploy/scripts/deploy_rpi.sh
+++ deploy/scripts/deploy_rpi.sh
@@ -5,14 +5,23 @@ project_dir="$(realpath "${script_dir}/../..")"
 
 remote_dir="~/MeshtasticAPI"
 
-rsync -av --exclude-from="${project_dir}/.rsyncignore" "${project_dir}/" paddy@meshcontrol.local:$remote_dir
+# Copy requirements.armv7.txt first
+scp "${project_dir}/requirements.armv7.txt" paddy@meshcontrol.local:$remote_dir
 
+# Run the pip install command on the remote server
+ssh paddy@meshcontrol.local << EOF
+    cd $remote_dir
+    source venv/bin/activate
+    pip install -r requirements.armv7.txt
+EOF
+
+# Rsync the rest of the project
+rsync -av --exclude-from="${project_dir}/.rsyncignore" "${project_dir}/" paddy@meshcontrol.local:$remote_dir
 
 # Run the migration script on the remote server
 ssh paddy@meshcontrol.local << EOF
     cd $remote_dir
     source venv/bin/activate
-    pip install -r requirements.armv7.txt
     cd MeshtasticBotManager
     python manage.py migrate
 EOF
