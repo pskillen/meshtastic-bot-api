c75478a0249c497045e160dcef59748607d805f5
Patrick Skillen <patrick.skillen2@nhs.scot>
2025-03-25 13:00:22 +0000

    fix: update meshtastic_id_to_hex to return ^all for broadcast ID

--- MeshtasticBotManager/common/mesh_node_helpers.py
+++ MeshtasticBotManager/common/mesh_node_helpers.py
@@ -1,10 +1,15 @@
 from datetime import datetime, timezone
 
+BROADCAST_ID = 0xffffffff
+
 
 def meshtastic_id_to_hex(meshtastic_id: int) -> str:
     """
     Convert a Meshtastic ID (integer form) to hex representation (!abcdef12)
     """
+    if meshtastic_id == BROADCAST_ID:
+        return "^all"
+
     return f"!{meshtastic_id:08x}"
 
 
@@ -12,6 +17,9 @@ def meshtastic_hex_to_int(node_id: str) -> int:
     """
     Convert a Meshtastic ID (hex representation) to integer form
     """
+    if node_id == "^all":
+        return BROADCAST_ID
+
     return int(node_id[1:], 16)
 
 
