224b89bf0382d39cc4b3e4d67770f36f58363a39
Patrick Skillen <patrick.skillen2@nhs.scot>
2026-03-05 12:19:21 +0000

    fix: update admin UI static files url, serve even when debug=false

--- MeshtasticBotManager/MeshtasticBotManager/settings.py
+++ MeshtasticBotManager/MeshtasticBotManager/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -148,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/admin/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "MeshtasticBotManager" / "static",
